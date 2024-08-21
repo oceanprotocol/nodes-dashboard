cac8550b5deadd2806c277016f016e5c41d26ac2
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-08-21 12:25:32 +0100

    Adding warning

--- src/components/Pages/Homepage/index.tsx
+++ src/components/Pages/Homepage/index.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic'
 import style from './style.module.css'
 import Table from '../../Table'
+import { Alert } from '@mui/material'
 
 const Map = dynamic(() => import('../../Map'), { ssr: false })
 
@@ -8,6 +9,7 @@ export default function HomePage() {
   return (
     <div className={style.root}>
       <Map />
+      <Alert severity="warning">Please note the the reward incetives provided for running a Node are in Alpha stage and it is possible they may change.</Alert>
       <Table />
     </div>
   )
