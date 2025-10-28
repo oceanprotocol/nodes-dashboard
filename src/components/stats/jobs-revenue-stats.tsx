fd3cf1985827fa973e5e0121885f82058660d398
Andrei Pintea <andrei.pintea@hpm.ro>
2025-10-28 13:42:39 +0200

    improve v bar chart

--- src/components/stats/jobs-revenue-stats.tsx
+++ src/components/stats/jobs-revenue-stats.tsx
@@ -10,30 +10,31 @@ const JobsRevenueStats = () => {
 
   return (
     <Card className={styles.root} paddingX="lg" paddingY="sm" radius="md" variant="glass-shaded">
-      <div className={styles.chartWrapper}>
+      <div className={styles.revenueWrapper}>
         <h3 className={styles.heading}>Total revenue</h3>
         <div className={styles.revenue}>
           <span className={styles.token}>OCEAN</span>{' '}
           <span className={styles.amount}>{formatNumber(totalRevenue)}</span>
         </div>
       </div>
-      <div className={styles.chartWrapper}>
-        <h3 className={styles.heading}>Revenue per epoch</h3>
-        <VBarChart
-          axisKey="epoch"
-          barKey="revenue"
-          chartType={ChartTypeEnum.REVENUE_PER_EPOCH}
-          data={revenuePerEpoch}
-        />
-      </div>
-      <div className={styles.chartWrapper}>
-        <h3 className={styles.heading}>Jobs per epoch</h3>
-        <VBarChart axisKey="epoch" barKey="jobs" chartType={ChartTypeEnum.JOBS_PER_EPOCH} data={jobsPerEpoch} />
-        <div className={styles.totalJobs}>
-          <div className={styles.label}>Total jobs</div>
-          <div className={styles.amount}>{formatNumber(totalJobs)}</div>
-        </div>
-      </div>
+      <VBarChart
+        axisKey="epoch"
+        barKey="revenue"
+        chartType={ChartTypeEnum.REVENUE_PER_EPOCH}
+        data={revenuePerEpoch}
+        title="Revenue per epoch"
+      />
+      <VBarChart
+        axisKey="epoch"
+        barKey="jobs"
+        chartType={ChartTypeEnum.JOBS_PER_EPOCH}
+        data={jobsPerEpoch}
+        footer={{
+          amount: formatNumber(totalJobs),
+          label: 'Total jobs',
+        }}
+        title="Jobs per epoch"
+      />
     </Card>
   );
 };
