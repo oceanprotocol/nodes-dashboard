90cdeaf2f491e00f6fa592ba3ec76f6d98b38601
Andrei Pintea <andrei.pintea@hpm.ro>
2025-10-28 16:26:59 +0200

    add gauge component

--- src/components/node-details/jobs-revenue-stats.tsx
+++ src/components/node-details/jobs-revenue-stats.tsx
@@ -1,5 +1,6 @@
 import Card from '@/components/card/card';
 import { ChartTypeEnum } from '@/components/chart/chart-type';
+import Gauge from '@/components/chart/gauge';
 import VBarChart from '@/components/chart/v-bar-chart';
 import { useStatsContext } from '@/context/stats-context';
 import { formatNumber } from '@/utils/formatters';
@@ -32,28 +33,8 @@ const JobsRevenueStats = () => {
           label: 'Total revenue',
         }}
       />
-      <VBarChart
-        axisKey="epoch"
-        barKey="jobs"
-        chartType={ChartTypeEnum.JOBS_PER_EPOCH}
-        data={jobsPerEpoch}
-        title="Jobs per epoch"
-        footer={{
-          amount: formatNumber(totalJobs),
-          label: 'Total jobs',
-        }}
-      />
-      <VBarChart
-        axisKey="epoch"
-        barKey="jobs"
-        chartType={ChartTypeEnum.JOBS_PER_EPOCH}
-        data={jobsPerEpoch}
-        title="Jobs per epoch"
-        footer={{
-          amount: formatNumber(totalJobs),
-          label: 'Total jobs',
-        }}
-      />
+      <Gauge label="Latest" max={5415} min={100} title="Benchmark results" value={3000} />
+      <Gauge label="Running" max={100} min={0} title="Queued jobs" value={63} />
       <VBarChart
         axisKey="epoch"
         barKey="jobs"
