143c3fbcea747e554a7f02b823eb787c3d94a475
andreip136 <129227833+andreip136@users.noreply.github.com>
2025-12-18 15:33:32 +0200

    add reusable stepper component

---
+++ src/components/stepper/stepper.tsx
@@ -0,0 +1,49 @@
+import Card from '@/components/card/card';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import classNames from 'classnames';
+import { useMemo } from 'react';
+import styles from './stepper.module.css';
+
+export type Step<T> = {
+  hidden?: boolean;
+  key: T;
+  label: string;
+};
+
+type StepperProps<T> = {
+  currentStep: T;
+  steps: Step<T>[];
+};
+
+const Stepper = <T,>({ currentStep, steps }: StepperProps<T>) => {
+  const visibleSteps = useMemo(() => steps.filter((step) => !step.hidden), [steps]);
+
+  const currentStepIndex = useMemo(
+    () => visibleSteps.findIndex((step) => step.key === currentStep),
+    [currentStep, visibleSteps]
+  );
+
+  const renderStep = (step: Step<T>, index: number) => {
+    const isActive = currentStepIndex === index;
+    const isComplete = currentStepIndex > index;
+    return (
+      <>
+        {index > 0 ? (
+          <div className={classNames(styles.separator, { [styles.active]: isActive || isComplete })}>—</div>
+        ) : null}
+        <div className={classNames(styles.step, { [styles.active]: isActive, [styles.complete]: isComplete })}>
+          {isComplete ? <CheckCircleOutlineIcon /> : null}
+          {step.label}
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <Card className={styles.root} variant="glass-shaded">
+      {visibleSteps.map((step, index) => renderStep(step, index))}
+    </Card>
+  );
+};
+
+export default Stepper;
