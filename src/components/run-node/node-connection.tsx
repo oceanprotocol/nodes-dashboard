0c2351c6dc3a8f936d282e14d3ba5f3ba74e6485
andreip136 <129227833+andreip136@users.noreply.github.com>
2025-12-18 17:32:16 +0200

    add node setup page content

---
+++ src/components/run-node/node-connection.tsx
@@ -0,0 +1,75 @@
+import Button from '@/components/button/button';
+import Card from '@/components/card/card';
+import Input from '@/components/input/input';
+import LinkIcon from '@mui/icons-material/Link';
+import LinkOffIcon from '@mui/icons-material/LinkOff';
+import classNames from 'classnames';
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import * as Yup from 'yup';
+import styles from './node-connection.module.css';
+
+type ConnectFormValues = {
+  nodeId: string;
+};
+
+const NodeConnection = () => {
+  // TODO replace with actual connection state
+  const [isConnected, setIsConnected] = useState(false);
+
+  const formik = useFormik<ConnectFormValues>({
+    initialValues: {
+      nodeId: '',
+    },
+    validationSchema: Yup.object().shape({
+      nodeId: Yup.string().required('Node ID is required'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+      setIsConnected(true);
+    },
+  });
+
+  const handleDisconnect = () => {
+    setIsConnected(false);
+  };
+
+  return (
+    <Card direction="column" padding="sm" radius="sm" spacing="sm" variant="glass">
+      <div className={styles.header}>
+        <h3>Connect to your node</h3>
+        <div className={classNames('chip', { chipError: !isConnected, chipSuccess: isConnected })}>
+          {isConnected ? 'Connected' : 'Not connected'}
+        </div>
+      </div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+      <Input
+        disabled={isConnected}
+        errorText={formik.touched.nodeId && formik.errors.nodeId ? formik.errors.nodeId : undefined}
+        label="Node ID"
+        name="nodeId"
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange}
+        type="text"
+        value={formik.values.nodeId}
+      />
+      {isConnected ? (
+        <Button
+          className="alignSelfEnd"
+          color="error"
+          contentBefore={<LinkOffIcon />}
+          onClick={handleDisconnect}
+          variant="outlined"
+        >
+          Disconnect
+        </Button>
+      ) : (
+        <Button className="alignSelfEnd" color="accent1" contentBefore={<LinkIcon />} onClick={formik.submitForm}>
+          Connect
+        </Button>
+      )}
+    </Card>
+  );
+};
+
+export default NodeConnection;
--- src/components/run-node/setup-page.tsx
+++ src/components/run-node/setup-page.tsx
@@ -1,4 +1,5 @@
 import Container from '@/components/container/container';
+import NodeSetup from '@/components/run-node/node-setup';
 import SectionTitle from '@/components/section-title/section-title';
 import { getRunNodeSteps, RunNodeStep } from '@/components/stepper/get-steps';
 import Stepper from '@/components/stepper/stepper';
@@ -12,8 +13,9 @@ const SetupPage = () => {
         subTitle="Description text"
         contentBetween={<Stepper<RunNodeStep> currentStep="setup" steps={getRunNodeSteps()} />}
       />
-
-      <div className="pageContentWrapper"></div>
+      <div className="pageContentWrapper">
+        <NodeSetup />
+      </div>
     </Container>
   );
 };
