eb840fa0bd19b80f618153048c2172ac7557b4c6
denisiuriet <iuriet.denis@gmail.com>
2026-01-05 14:33:28 +0200

    remove hardcoded values

--- src/components/button/download-logs-button.tsx
+++ src/components/button/download-logs-button.tsx
@@ -27,96 +27,14 @@ export const DownloadLogsButton = ({ job }: DownloadLogsButtonProps) => {
 
     setError(null);
 
-    job = {
-      owner: '0xD8127C2896F6D6aB77aa7F89fa4eA4a45a802EB5',
-      did: null,
-      jobId: 'b76a5000261497a7082e4206209c61f7ffe683b2547b92104dabce7513fd824a',
-      dateCreated: '1767600980.248',
-      dateFinished: '1767600992.647',
-      status: 70,
-      statusText: 'Job finished',
-      results: [
-        {
-          filename: 'image.log',
-          filesize: 639,
-          type: 'imageLog',
-          index: 0,
-        },
-        {
-          filename: 'configuration.log',
-          filesize: 324,
-          type: 'configurationLog',
-          index: 1,
-        },
-        {
-          filename: 'algorithm.log',
-          filesize: 73,
-          type: 'algorithmLog',
-          index: 2,
-        },
-        {
-          filename: 'outputs.tar',
-          filesize: 2560,
-          type: 'output',
-          index: 3,
-        },
-      ],
-      inputDID: null,
-      algoDID: null,
-      agreementId: null,
-      environment:
-        '0x14fc9dabe8b4a19310a3a0bf976fb14ab1a6c1f77f5ac74d35211f969c5180eb-0x12dcecf35edca64893b4e7d5fe81b2c22e4d6e38366ec4a57348037cc8c62123',
-      stopRequested: false,
-      resources: [
-        {
-          id: 'cpu',
-          amount: 1,
-        },
-        {
-          id: 'ram',
-          amount: 1,
-        },
-        {
-          id: 'disk',
-          amount: 1,
-        },
-        {
-          id: 'myGPU',
-          amount: 1,
-        },
-      ],
-      isFree: true,
-      algoStartTimestamp: '1767600985.589',
-      algoStopTimestamp: '1767600990.613',
-      terminationDetails: {
-        exitCode: 0,
-        OOMKilled: false,
-      },
-      payment: null,
-      algoDuration: 5.023999929428101,
-      queueMaxWaitTime: 0,
-      maxJobDuration: 7200,
-    } as any;
-
     try {
       const jobId = job.environment.split('-')[0] + '-' + job.jobId;
-      //const authToken = await generateAuthTokenWithSmartAccount(job.peerId, account.address, signMessageAsync)
-      const authToken = await generateAuthTokenWithSmartAccount(
-        '16Uiu2HAmR9z4EhF9zoZcErrdcEJKCjfTpXJfBcmbNppbT3QYtBpi',
-        account.address,
-        signMessageAsync
-      );
+
+      const authToken = await generateAuthTokenWithSmartAccount(job.peerId, account.address, signMessageAsync);
 
       const logFiles = job.results.filter((result: any) => result.filename.includes('.log'));
-      //const logPromises = logIndexes.map((index: any) => getComputeResult(job.peerId, job.jobId, index, authToken, account.address));
       const logPromises = logFiles.map((logFile: any) =>
-        getComputeResult(
-          '16Uiu2HAmR9z4EhF9zoZcErrdcEJKCjfTpXJfBcmbNppbT3QYtBpi',
-          jobId,
-          logFile.index,
-          authToken,
-          account.address
-        )
+        getComputeResult(job.peerId, jobId, logFile.index, authToken, account.address)
       );
 
       const downloadedLogs = await Promise.all(logPromises);
@@ -137,7 +55,7 @@ export const DownloadLogsButton = ({ job }: DownloadLogsButtonProps) => {
       const url = URL.createObjectURL(zipBlob);
       const link = document.createElement('a');
       link.href = url;
-      link.download = `logs.zip`;
+      link.download = `logs-${job.jobId}.zip`;
 
       document.body.appendChild(link);
       link.click();
--- src/components/button/download-result-button.tsx
+++ src/components/button/download-result-button.tsx
@@ -26,98 +26,17 @@ export const DownloadResultButton = ({ job }: DownloadResultButtonProps) => {
 
     setError(null);
 
-    job = {
-      owner: '0xD8127C2896F6D6aB77aa7F89fa4eA4a45a802EB5',
-      did: null,
-      jobId: 'b76a5000261497a7082e4206209c61f7ffe683b2547b92104dabce7513fd824a',
-      dateCreated: '1767600980.248',
-      dateFinished: '1767600992.647',
-      status: 70,
-      statusText: 'Job finished',
-      results: [
-        {
-          filename: 'image.log',
-          filesize: 639,
-          type: 'imageLog',
-          index: 0,
-        },
-        {
-          filename: 'configuration.log',
-          filesize: 324,
-          type: 'configurationLog',
-          index: 1,
-        },
-        {
-          filename: 'algorithm.log',
-          filesize: 73,
-          type: 'algorithmLog',
-          index: 2,
-        },
-        {
-          filename: 'outputs.tar',
-          filesize: 2560,
-          type: 'output',
-          index: 3,
-        },
-      ],
-      inputDID: null,
-      algoDID: null,
-      agreementId: null,
-      environment:
-        '0x14fc9dabe8b4a19310a3a0bf976fb14ab1a6c1f77f5ac74d35211f969c5180eb-0x12dcecf35edca64893b4e7d5fe81b2c22e4d6e38366ec4a57348037cc8c62123',
-      stopRequested: false,
-      resources: [
-        {
-          id: 'cpu',
-          amount: 1,
-        },
-        {
-          id: 'ram',
-          amount: 1,
-        },
-        {
-          id: 'disk',
-          amount: 1,
-        },
-        {
-          id: 'myGPU',
-          amount: 1,
-        },
-      ],
-      isFree: true,
-      algoStartTimestamp: '1767600985.589',
-      algoStopTimestamp: '1767600990.613',
-      terminationDetails: {
-        exitCode: 0,
-        OOMKilled: false,
-      },
-      payment: null,
-      algoDuration: 5.023999929428101,
-      queueMaxWaitTime: 0,
-      maxJobDuration: 7200,
-    } as any;
-
     try {
       const jobId = job.environment.split('-')[0] + '-' + job.jobId;
       setIsDownloading(true);
       const archive = job.results.find((result: any) => result.filename.includes('.tar'));
 
-      const authToken = await generateAuthTokenWithSmartAccount(
-        '16Uiu2HAmR9z4EhF9zoZcErrdcEJKCjfTpXJfBcmbNppbT3QYtBpi',
-        account.address,
-        signMessageAsync
-      );
+      const authToken = await generateAuthTokenWithSmartAccount(job.peerId, account.address, signMessageAsync);
 
-      const result = await getComputeResult(
-        '16Uiu2HAmR9z4EhF9zoZcErrdcEJKCjfTpXJfBcmbNppbT3QYtBpi',
-        jobId,
-        archive?.index,
-        authToken,
-        account.address
-      );
+      const result = await getComputeResult(job.peerId, jobId, archive?.index, authToken, account.address);
       if (result instanceof Uint8Array) {
         if (result.byteLength === 0) {
-          console.error('Received empty response (0 bytes). Skipping download.');
+          console.log('Received empty response (0 bytes). Skipping download.');
           return;
         }
 
