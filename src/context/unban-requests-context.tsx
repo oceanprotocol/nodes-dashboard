f526f224754f678bc21ce4fec8c3e04c6ff54473
Andrei Pop <90789089+ndrpp@users.noreply.github.com>
2026-01-08 16:04:55 +0200

    Feat/127 get push node config node details (#231)
    * payment redesign
    
    * auto loading button
    
    * update loading
    
    * payment loading
    
    * update loading
    
    * sign and open vscode
    
    * fix build
    
    * alchemy integration
    
    * hide wallet button
    
    * open editor
    
    * update vscode icon
    
    * fix build
    
    * fetch token symbol
    
    * update token symbol logic
    
    * feat: add basic "send gas fees" functionality
    
    * feat: start base for withdraw
    
    * fix: improve withdraw modal styling
    
    * feat: extend alchemy session time to 1 day | cleanup balance.tsx
    
    * feat: add fetch & push config functions in p2p context
    
    * feat: enable unban requests calls temporarily
    
    * feat: update admin requests
    
    * chore: cleanup
    
    * feat: install json editor
    
    * feat: improve modal with json editor & flows
    
    * fix: type issue
    
    * fix: modal window centering
    
    * feat: switch to Dialog from material UI
    
    * chore: remove console.log
    
    * chore: cleanup
    
    * fix scroll
    
    * bump mui version
    
    * fix: code review
    
    ---------
    
    Co-authored-by: andreip136 <129227833+andreip136@users.noreply.github.com>

--- src/context/unban-requests-context.tsx
+++ src/context/unban-requests-context.tsx
@@ -7,7 +7,7 @@ type UnbanRequestsContextType = {
   unbanRequests: UnbanRequest[];
 
   fetchUnbanRequests: (nodeId: string) => Promise<void>;
-  requestNodeUnban: (nodeId: string, signature: string, expiryTimestamp: number) => Promise<void>;
+  requestNodeUnban: (nodeId: string, signature: string, expiryTimestamp: number, address: string) => Promise<void>;
 };
 
 const UnbanRequestsContext = createContext<UnbanRequestsContextType | undefined>(undefined);
@@ -29,16 +29,20 @@ export const UnbanRequestsProvider = ({ children }: { children: ReactNode }) => 
     }
   }, []);
 
-  const requestNodeUnban = useCallback(async (nodeId: string, signature: string, expiryTimestamp: number) => {
-    try {
-      await axios.post(`${getApiRoute('nodeUnbanRequests')}/${nodeId}/unban`, {
-        signature,
-        expiryTimestamp,
-      });
-    } catch (error) {
-      console.error('Error requesting node unban: ', error);
-    }
-  }, []);
+  const requestNodeUnban = useCallback(
+    async (nodeId: string, signature: string, expiryTimestamp: number, address: string) => {
+      try {
+        await axios.post(`${getApiRoute('nodeUnbanRequests')}/${nodeId}/unban`, {
+          signature,
+          expiryTimestamp,
+          address,
+        });
+      } catch (error) {
+        console.error('Error requesting node unban: ', error);
+      }
+    },
+    []
+  );
 
   return (
     <UnbanRequestsContext.Provider
