fcac1ba9f008f4713ac05d33dc6f8aca1e95241b
giurgiur99 <giurgiur99@gmail.com>
2026-01-19 15:35:51 +0200

    sni peer

--- src/shared/consts/bootstrapNodes.ts
+++ src/shared/consts/bootstrapNodes.ts
@@ -13,5 +13,8 @@ export const OCEAN_BOOTSTRAP_NODES = [
   //'/dns6/node4.oceanprotocol.com/tcp/9003/ws/p2p/16Uiu2HAmSTVTArioKm2wVcyeASHYEsnx2ZNq467Z4GMDU4ErEPom',
   // compute 1
   // '/dns4/compute1.oceanprotocol.com/tcp/9005/wss/p2p/16Uiu2HAmR9z4EhF9zoZcErrdcEJKCjfTpXJfBcmbNppbT3QYtBpi',
-  '/ip4/34.107.108.64/tcp/9001/ws/p2p/16Uiu2HAmUf4JpduE6CXpNMm1xdjhFUH53G4c9o37Kat3wsreUyaQ',
+  // '/ip4/34.107.108.64/tcp/9001/ws/p2p/16Uiu2HAmUf4JpduE6CXpNMm1xdjhFUH53G4c9o37Kat3wsreUyaQ',
+  // "/ip4/198.145.104.8/tcp/9001/tls/sni/198-145-104-8.kzwfwjn5ji4puuok23h2yyzro0fe1rqv1bqzbmrjf7uqyj504rawjl4zs68mepr.libp2p.direct/ws/p2p/16Uiu2HAmR9z4EhF9zoZcErrdcEJKCjfTpXJfBcmbNppbT3QYtBpi"
+  '/dns4/198-145-104-8.kzwfwjn5ji4puuok23h2yyzro0fe1rqv1bqzbmrjf7uqyj504rawjl4zs68mepr.libp2p.direct/tcp/9001/wss/p2p/16Uiu2HAmR9z4EhF9zoZcErrdcEJKCjfTpXJfBcmbNppbT3QYtBpi'
+  
 ];
