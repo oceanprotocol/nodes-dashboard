33f9a5a238b2128b31530552914afb0c37d1c39a
ndrpp <popandrei230@yahoo.com>
2026-01-28 12:32:34 +0200

    fix: update sign message function for EOA

--- src/lib/sign-message.ts
+++ src/lib/sign-message.ts
@@ -1,15 +1,13 @@
-import { ethers, JsonRpcSigner, Signer, toUtf8Bytes } from 'ethers';
+import { JsonRpcSigner, Signer } from 'ethers';
 
 export async function signMessage(message: string, signer: Signer): Promise<string> {
-  const consumerMessage = ethers.keccak256(toUtf8Bytes(message));
-  const messageHashBytes = ethers.getBytes(consumerMessage);
   try {
-    return await signer.signMessage(messageHashBytes);
+    return await signer.signMessage(message);
   } catch (error) {
     const network = await signer?.provider?.getNetwork();
     const chainId = Number(network?.chainId);
     if (chainId === 8996) {
-      return await (signer as JsonRpcSigner)._legacySignMessage(messageHashBytes);
+      return await (signer as JsonRpcSigner)._legacySignMessage(message);
     }
     throw error;
   }
