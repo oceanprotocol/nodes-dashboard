14dc1752e9f45d0ecbb7aa5b1e00dfc5dcbfd5d4
ndrpp <popandrei230@yahoo.com>
2026-02-18 16:56:12 +0200

    fix: check cookiebot consent before starting posthog

--- src/lib/use-posthog.tsx
+++ src/lib/use-posthog.tsx
@@ -4,18 +4,32 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import { useEffect } from 'react'
 
+declare global {
+  interface Window {
+    Cookiebot?: {
+      consent?: {
+        statistics?: boolean
+      }
+    }
+  }
+}
+
 export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init("phc_hD7bhooFbRUWqSWOvRAZiHv4tr6mYYgleeWGkQ52eWD", {
-      api_host: "https://eu.i.posthog.com",
-      defaults: '2026-01-30',
-      capture_exceptions: true,
-    })
-    //posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    //  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    //  defaults: '2026-01-30',
-    //  capture_exceptions: true,
-    //})
+    const initPostHog = () => {
+      if (window.Cookiebot?.consent?.statistics) {
+        posthog.init("phc_hD7bhooFbRUWqSWOvRAZiHv4tr6mYYgleeWGkQ52eWD", {
+          api_host: "https://eu.i.posthog.com",
+          defaults: '2026-01-30',
+          capture_exceptions: true,
+        })
+      }
+    }
+
+    initPostHog()
+
+    window.addEventListener('CookiebotOnAccept', initPostHog)
+    return () => window.removeEventListener('CookiebotOnAccept', initPostHog)
   }, [])
 
   return (
