eb76b117fca70d1951286961cd4c0950f60f06d0
ndrpp <popandrei230@yahoo.com>
2026-02-18 15:31:42 +0200

    chore: use hardcoded env for testing

--- src/lib/use-posthog.tsx
+++ src/lib/use-posthog.tsx
@@ -6,11 +6,16 @@ import { useEffect } from 'react'
 
 export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    posthog.init("phc_hD7bhooFbRUWqSWOvRAZiHv4tr6mYYgleeWGkQ52eWD", {
+      api_host: "https://eu.i.posthog.com",
       defaults: '2026-01-30',
       capture_exceptions: true,
     })
+    //posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    //  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    //  defaults: '2026-01-30',
+    //  capture_exceptions: true,
+    //})
   }, [])
 
   return (
