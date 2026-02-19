9e543860d5bf1481ba14f3d2ef82f22fec5673dc
ndrpp <popandrei230@yahoo.com>
2026-02-19 10:55:57 +0200

    fix: build issue

--- src/lib/use-posthog.tsx
+++ src/lib/use-posthog.tsx
@@ -1,45 +1,45 @@
-'use client'
+'use client';
 
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
-import { useEffect } from 'react'
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+import { useEffect } from 'react';
 
 declare global {
   interface Window {
     Cookiebot?: {
       consent?: {
-        statistics?: boolean
-      }
-    }
+        statistics?: boolean;
+      };
+    };
   }
 }
 
 export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    const initPostHog = () => {
+    function initPostHog() {
       if (window.Cookiebot?.consent?.statistics) {
-        posthog.init("phc_hD7bhooFbRUWqSWOvRAZiHv4tr6mYYgleeWGkQ52eWD", {
-          api_host: "https://eu.i.posthog.com",
+        posthog.init('phc_hD7bhooFbRUWqSWOvRAZiHv4tr6mYYgleeWGkQ52eWD', {
+          api_host: 'https://eu.i.posthog.com',
           defaults: '2026-01-30',
           capture_exceptions: true,
-          enable_heatmaps: true
-        })
+          enable_heatmaps: true,
+        });
       }
     }
 
-    initPostHog()
-
-    window.addEventListener('CookiebotOnAccept', initPostHog)
-    window.addEventListener('CookiebotOnDecline', posthog.opt_out_capturing())
-    return () => {
-        window.removeEventListener('CookiebotOnAccept', initPostHog)
-        window.removeEventListener('CookiebotOnDecline', posthog.opt_out_capturing())
+    function optOutCapturing() {
+      return posthog.opt_out_capturing();
     }
-  }, [])
 
-  return (
-      <PostHogProvider client={posthog}>
-        {children}
-      </PostHogProvider>
-  );
+    initPostHog();
+
+    window.addEventListener('CookiebotOnAccept', initPostHog);
+    window.addEventListener('CookiebotOnDecline', optOutCapturing);
+    return () => {
+      window.removeEventListener('CookiebotOnAccept', initPostHog);
+      window.removeEventListener('CookiebotOnDecline', optOutCapturing);
+    };
+  }, []);
+
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
