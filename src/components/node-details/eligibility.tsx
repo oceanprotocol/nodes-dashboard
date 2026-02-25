a17f792987721cddefcf2a31ba4604c739cb04ff
andreip136 <129227833+andreip136@users.noreply.github.com>
2026-02-25 18:06:29 +0200

    update eligibility design

--- src/components/node-details/eligibility.tsx
+++ src/components/node-details/eligibility.tsx
@@ -16,20 +16,22 @@ const Eligibility = ({ eligibility, eligibilityCauseStr, banInfo }: EligibilityP
   switch (eligibility) {
     case NodeEligibility.ELIGIBLE:
       return (
-        <Card className={styles.root} padding="sm" radius="md" shadow="success" variant="success">
-          <CheckCircleOutlineIcon className={styles.icon} />
-          <h3>Eligible</h3>
-          <div className={styles.description}>This node is active and can receive rewards</div>
+        <Card direction="column" padding="sm" radius="md" shadow="success" spacing="sm" variant="success">
+          <div className={styles.header}>
+            <CheckCircleOutlineIcon className={styles.icon} />
+            <h3>Eligible</h3>
+          </div>
+          <div>This node is active and can receive rewards</div>
         </Card>
       );
     case NodeEligibility.NON_ELIGIBLE:
       return (
-        <Card className={styles.root} padding="sm" radius="md" shadow="warning" variant="warning">
-          <ErrorOutlineIcon className={styles.icon} />
-          <h3>Not eligible</h3>
-          <div className={styles.description}>
-            This node is active, but does not meet the criteria for receiving rewards
+        <Card direction="column" padding="sm" radius="md" shadow="warning" spacing="sm" variant="warning">
+          <div className={styles.header}>
+            <ErrorOutlineIcon className={styles.icon} />
+            <h3>Not eligible</h3>
           </div>
+          <div>This node is active, but does not meet the criteria for receiving rewards</div>
           <div className={styles.reason}>
             <strong className="alignSelfStart">Reason:</strong> <span>{eligibilityCauseStr ?? 'Unknown'}</span>
           </div>
@@ -37,10 +39,12 @@ const Eligibility = ({ eligibility, eligibilityCauseStr, banInfo }: EligibilityP
       );
     case NodeEligibility.BANNED:
       return (
-        <Card className={styles.root} padding="sm" radius="md" shadow="error" variant="error">
-          <HighlightOffIcon className={styles.icon} />
-          <h3>Banned</h3>
-          <div className={styles.description}>This node is excluded from all operations and rewards</div>
+        <Card direction="column" padding="sm" radius="md" shadow="error" spacing="sm" variant="error">
+          <div className={styles.header}>
+            <HighlightOffIcon className={styles.icon} />
+            <h3>Banned</h3>
+          </div>
+          <div>This node is excluded from all operations and rewards</div>
           <div className={styles.reason}>
             <strong className="alignSelfStart">Reason:</strong> <span>{banInfo?.reason ?? 'Unknown'}</span>
           </div>
