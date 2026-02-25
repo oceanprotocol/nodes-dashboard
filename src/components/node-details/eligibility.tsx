159336876823d3cced1598dee4ca151681ee4b71
andreip136 <129227833+andreip136@users.noreply.github.com>
2026-02-25 18:01:49 +0200

    update eligibility design

--- src/components/node-details/eligibility.tsx
+++ src/components/node-details/eligibility.tsx
@@ -12,13 +12,14 @@ type EligibilityProps = {
 };
 
 const Eligibility = ({ eligibility, eligibilityCauseStr, banInfo }: EligibilityProps) => {
+  console.log(eligibilityCauseStr);
   switch (eligibility) {
     case NodeEligibility.ELIGIBLE:
       return (
         <Card className={styles.root} padding="sm" radius="md" shadow="success" variant="success">
           <CheckCircleOutlineIcon className={styles.icon} />
           <h3>Eligible</h3>
-          <div className={styles.content}>This node is active and can receive rewards</div>
+          <div className={styles.description}>This node is active and can receive rewards</div>
         </Card>
       );
     case NodeEligibility.NON_ELIGIBLE:
@@ -26,9 +27,11 @@ const Eligibility = ({ eligibility, eligibilityCauseStr, banInfo }: EligibilityP
         <Card className={styles.root} padding="sm" radius="md" shadow="warning" variant="warning">
           <ErrorOutlineIcon className={styles.icon} />
           <h3>Not eligible</h3>
-          <div className={styles.content}>
+          <div className={styles.description}>
             This node is active, but does not meet the criteria for receiving rewards
-            <strong>Reason:</strong> <span>{eligibilityCauseStr ?? '-'}</span>
+          </div>
+          <div className={styles.reason}>
+            <strong className="alignSelfStart">Reason:</strong> <span>{eligibilityCauseStr ?? 'Unknown'}</span>
           </div>
         </Card>
       );
@@ -37,12 +40,9 @@ const Eligibility = ({ eligibility, eligibilityCauseStr, banInfo }: EligibilityP
         <Card className={styles.root} padding="sm" radius="md" shadow="error" variant="error">
           <HighlightOffIcon className={styles.icon} />
           <h3>Banned</h3>
-          <div className={styles.content}>
-            <div>
-              This node is excluded from all operations and rewards
-              <br />
-              <strong> Reason:</strong> <span> {banInfo?.reason ?? 'Unknown'}</span>
-            </div>
+          <div className={styles.description}>This node is excluded from all operations and rewards</div>
+          <div className={styles.reason}>
+            <strong className="alignSelfStart">Reason:</strong> <span>{banInfo?.reason ?? 'Unknown'}</span>
           </div>
         </Card>
       );
