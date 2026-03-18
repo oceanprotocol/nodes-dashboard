417296cd6a811fdcf77b2d730d9170682a127075
andreip136 <129227833+andreip136@users.noreply.github.com>
2026-03-18 12:45:13 +0200

    update docs style

--- src/components/docs/docs-widget.tsx
+++ src/components/docs/docs-widget.tsx
@@ -1,22 +1,24 @@
+import Button from '@/components/button/button';
+import { Collapse } from '@mui/material';
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import styles from './docs-widget.module.css';
 
 const GitBookFrame = dynamic(() => import('@gitbook/embed/react').then((mod) => mod.GitBookFrame), { ssr: false });
 
 const DocsWidget: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <GitBookFrame
-      trademark={false}
-      tabs={['assistant', 'docs']}
-      greeting={{ title: 'Welcome!', subtitle: 'How can I help?' }}
-      suggestions={['What is GitBook?', 'How do I get started?']}
-      actions={[
-        {
-          icon: 'circle-question',
-          label: 'Contact Support',
-          onClick: () => {},
-        },
-      ]}
-    />
+    <div className={styles.root}>
+      <Collapse className={styles.collapse} in={isOpen}>
+        <GitBookFrame className={styles.frame} />
+      </Collapse>
+      {/* {isOpen ? <GitBookFrame className={styles.frame} /> : null} */}
+      <Button color="primary-inverse" onClick={() => setIsOpen(!isOpen)} variant="glass">
+        Docs
+      </Button>
+    </div>
   );
 };
 
