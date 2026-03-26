b7aa66eb79cc45590e721a89f02250ab1946868b
andreip136 <129227833+andreip136@users.noreply.github.com>
2026-03-26 14:30:19 +0200

    add Show More button in envs list from node details page

--- src/components/node-details/environments.tsx
+++ src/components/node-details/environments.tsx
@@ -1,10 +1,12 @@
+import Button from '@/components/button/button';
 import Card from '@/components/card/card';
 import EnvironmentCard from '@/components/environment-card/environment-card';
 import { CHAIN_ID } from '@/constants/chains';
 import { getSupportedTokens } from '@/constants/tokens';
 import { useP2P } from '@/contexts/P2PContext';
 import { EnvNodeInfo } from '@/types/environments';
-import { useMemo } from 'react';
+import { Collapse } from '@mui/material';
+import { useMemo, useState } from 'react';
 import styles from './environments.module.css';
 
 type EnvironmentsProps = {
@@ -14,26 +16,33 @@ type EnvironmentsProps = {
 const Environments = ({ nodeInfo }: EnvironmentsProps) => {
   const { isReady, envs } = useP2P();
 
-  // Only show environments that support chain ID and USDC/COMPY
-  const filteredEnvs = useMemo(
-    () =>
-      envs.filter((env) => {
-        if (!env.fees?.[CHAIN_ID]) {
-          return false;
-        }
-        if (
-          !env.fees[CHAIN_ID].some(
-            (fee) =>
-              fee.feeToken.toLowerCase() === getSupportedTokens().COMPY.address.toLowerCase() ||
-              fee.feeToken.toLowerCase() === getSupportedTokens().USDC.address.toLowerCase()
-          )
-        ) {
-          return false;
-        }
-        return true;
-      }),
-    [envs]
-  );
+  const [showingMore, setShowingMore] = useState(false);
+
+  /**
+   * Show only the first 3 environments
+   * Show rest of the environments in a collapsible section
+   */
+  const [firstEnvs, restEnvs] = useMemo(() => {
+    // Only show environments that support chain ID and USDC/COMPY
+    const filteredEnvs = envs.filter((env) => {
+      if (!env.fees?.[CHAIN_ID]) {
+        return false;
+      }
+      if (
+        !env.fees[CHAIN_ID].some(
+          (fee) =>
+            fee.feeToken.toLowerCase() === getSupportedTokens().COMPY.address.toLowerCase() ||
+            fee.feeToken.toLowerCase() === getSupportedTokens().USDC.address.toLowerCase()
+        )
+      ) {
+        return false;
+      }
+      return true;
+    });
+    const firstEnvs = filteredEnvs.slice(0, 3);
+    const restEnvs = filteredEnvs.slice(3);
+    return [firstEnvs, restEnvs];
+  }, [envs]);
 
   return (
     <Card direction="column" padding="md" radius="lg" shadow="black" spacing="md" variant="glass-shaded">
@@ -41,8 +50,43 @@ const Environments = ({ nodeInfo }: EnvironmentsProps) => {
       <div className={styles.list}>
         {!isReady ? (
           <div>Fetching data...</div>
-        ) : filteredEnvs.length ? (
-          filteredEnvs.map((env) => <EnvironmentCard key={env.id} environment={env} nodeInfo={nodeInfo} />)
+        ) : firstEnvs.length ? (
+          <>
+            {firstEnvs.map((env) => (
+              <EnvironmentCard key={env.id} environment={env} nodeInfo={nodeInfo} />
+            ))}
+            {restEnvs.length ? (
+              <>
+                {showingMore ? null : (
+                  <Button
+                    className="alignSelfCenter"
+                    color="accent2"
+                    onClick={() => setShowingMore(true)}
+                    variant="filled"
+                  >
+                    Show more
+                  </Button>
+                )}
+                <Collapse in={showingMore}>
+                  <div className={styles.list}>
+                    {restEnvs.map((env) => (
+                      <EnvironmentCard key={env.id} environment={env} nodeInfo={nodeInfo} />
+                    ))}
+                    {showingMore ? (
+                      <Button
+                        className="alignSelfCenter"
+                        color="accent2"
+                        onClick={() => setShowingMore(false)}
+                        variant="filled"
+                      >
+                        Show less
+                      </Button>
+                    ) : null}
+                  </div>
+                </Collapse>
+              </>
+            ) : null}
+          </>
         ) : (
           <div>No environments</div>
         )}
